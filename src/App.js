d10de397045c9c72ba60eef14ab836097a12c551
teodora <tbd@shippii.com>
2022-08-01 16:33:33 +0300

    Start new project,Pages and routes, Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Explore from "./pages/Explore";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SingIn from "./pages/SingIn";
+import SingUp from "./pages/SingUp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/singIn" element={<SingIn />} />
+          <Route path="/singUp" element={<SingUp />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/forgotenPass" element={<ForgotPassword />} />
+        </Routes>
+        <Navbar/>
+      </Router>
+
+    
+
+   
+    </>
   );
 }
 
---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_bbh--XoKBNGTgW8AIBDULdkPjZ7R0Q0",
+  authDomain: "sbr-realestate.firebaseapp.com",
+  projectId: "sbr-realestate",
+  storageBucket: "sbr-realestate.appspot.com",
+  messagingSenderId: "1060569585652",
+  appId: "1:1060569585652:web:aacf2f747c163e4d888ba1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFireStore();
