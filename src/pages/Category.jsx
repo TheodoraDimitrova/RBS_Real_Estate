dd0c19961328885cf6a124c406ddcf716e346bec
teodora <tbd@shippii.com>
2022-08-20 19:21:38 +0300

    Fetch Advertisements from firebase

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,5 +1,75 @@
-import React from "react";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+  where,
+} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import Spinner from "../components/Spinner";
+import { db } from "../firebase.config";
 
 export default function Category() {
-  return <div>Category</div>;
+  const [listings, SetListings] = useState(null);
+  const [loading, SetLoading] = useState(true);
+  const params = useParams();
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        const q = query(
+          collection(db, "listings"),
+          where("type", "==", params.categoryName),
+          orderBy("timestamp", "desc"),
+          limit(10)
+        );
+        const querySnapshot = await getDocs(q);
+        const listings = [];
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          //   console.log(doc.id, " => ", doc.data());
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        SetListings(listings);
+        SetLoading(false);
+      } catch (error) {
+        toast.error("Something went wrong");
+      }
+    };
+
+    fetchListings();
+  }, []);
+
+  return (
+    <div className="category">
+      <header>
+        <p className="pageHeader">
+          {params.categoryName === "rent"
+            ? "Places for Rent"
+            : "Places for Sale"}
+        </p>
+      </header>
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className="categoryListings">
+              {listings.map((item, index) => (
+                <h3 key="index">{item.data.name}</h3>
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>No Advertisements for {params.categoryName}</p>
+      )}
+    </div>
+  );
 }
