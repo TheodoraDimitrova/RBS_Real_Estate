b5b3eb19a74da854396aac1c5aed5a4a6e9e0146
teodora <tbd@shippii.com>
2022-08-22 14:22:32 +0300

    ad details page

--- src/pages/AdPage.jsx
+++ src/pages/AdPage.jsx
@@ -21,8 +21,8 @@ function AdPage() {
       const docSnap = await getDoc(docRef);
 
       if (docSnap.exists()) {
-        console.log(docSnap.data());
         setAd(docSnap.data());
+
         SetLoading(false);
       }
     };
@@ -30,7 +30,63 @@ function AdPage() {
     fetchAd();
   }, [navigate, params.id]);
 
-  return <div>AdPage</div>;
+  if (loading) return <Spinner />;
+
+  return (
+    <main>
+      <div
+        className="shareIconDiv"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          SetShareLink(true);
+
+          setTimeout(() => {
+            SetShareLink(false);
+          }, 2000);
+        }}
+      >
+        <img src={shareIcom} alt="share-icon" />
+      </div>
+      {shareLink && <p className="linkCopied">Link is copied!</p>}
+      <div className="listingDetails">
+        <p className="listingName">
+          {ad.name} -{" "}
+          {ad.offer
+            ? ad.discoundPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            : ad.regularPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
+          ЛВ.
+        </p>
+        <p className="listingType btn-grad">
+          {ad.type === "rent" ? "Rent" : "Sale"}
+        </p>
+        {ad.offer && (
+          <p className="discountPrice">
+            `${ad.regularPrice}- ${ad.discoundPrice}` discount
+          </p>
+        )}
+        <ul className="listingDetailsList">
+          <li>{ad.bedrooms > 1 ? `${ad.bedrooms} Bedrooms` : "1 Bedroom"}</li>
+          <li>
+            {ad.bathrooms > 1 ? `${ad.bathroom} Bathrooms` : "1 Bathroom"}
+          </li>
+          <li>{ad.parking && "Parking Spot"}</li>
+          <li>{ad.furnished && "Furnished"}</li>
+        </ul>
+        {ad.useRef}-------
+        {auth.currentUser.uid}
+        {auth.currentUser.uid !== ad.useRef && (
+          <Link
+            className="btn-grad"
+            to={`/contact/${ad.useRef}?adName=${ad.name}`}
+          >
+            Contact Owner
+          </Link>
+        )}
+      </div>
+    </main>
+  );
 }
 
 export default AdPage;
